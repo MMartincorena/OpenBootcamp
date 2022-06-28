e4cabd128283e85fc59b342de27ca6357f73d6d8
Martin Martincorena <martin.martincorena@sofka.com.co>
2022-06-28 17:07:45 -0300

    Realizado ejercicio2

---
+++ Modulo2_Sintaxis_Variables_PReservadas/ejercicio2.js
@@ -0,0 +1,12 @@
+const Datos = [
+  (nombre = "Martin"),
+  (edad = 29),
+  (desarrollador = true),
+  (fNacimiento = 09 / 07 / 1992),
+  (libroFavorito = {
+    titulo: "La divina comedia",
+    autor: "Dante Alighieri",
+    fecha: 1304,
+    url: "https://es.wikipedia.org/wiki/Divina_comedia",
+  }),
+];
