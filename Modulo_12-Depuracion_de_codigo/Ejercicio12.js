02bad5559bd4d0c1026de9e9e86e4fd3ed74ace9
Martin Martincorena <martin.martincorena@sofka.com.co>
2022-07-04 15:58:29 -0300

    Terminado - Ejercicio12

---
+++ Modulo_12-Depuracion_de_codigo/Ejercicio12.js
@@ -0,0 +1,17 @@
+/* 
+Crea un nuevo fichero JS que contenga las siguientes líneas
+
+- Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+- Ejecuta la depuración de VSCode para visualizar la ejecución de la función */
+
+function GenerarFibonacci(number) {
+  let fibonacci = [];
+  fibonacci[0] = 0;
+  fibonacci[1] = 1;
+  for (let i = 2; i < number; i++) {
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+  }
+  return fibonacci;
+}
+
+console.log(GenerarFibonacci(10));
