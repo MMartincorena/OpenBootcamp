5eaec160d152e17d8547ad04fc8643259bbb613b
Martin Martincorena <martin.martincorena@sofka.com.co>
2022-07-06 11:20:06 -0300

    Ejercicios nuevo subidos

---
+++ Ejemplos/ejemplos.js
@@ -0,0 +1,19 @@
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result);
+
+//////////////////////////////////////
+
+var numbers = [1, 5, 10, 15];
+var doubles = numbers.map((x) => x * 2);
+
+console.log(doubles);
